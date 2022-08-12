bf25bec5a5a36482daf9173a879ce2772ce2c72a
AntonShpakovych <shpakovych18@gmail.com>
2022-08-12 17:41:47 +0300

    step1

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe 'Home page', type: :feature do
   it 'User open site and see home page' do
     expect(page).to have_text(t('home.partials.welcome.welcome_title'))
     expect(page).to have_text(t('home.partials.welcome.welcome_description'))
-    # expect(page).to have_text(t('.title_best_sellers'))
-    # expect(page).to have_text(t('.title_best_sellers'))
-    # expect(page.has_button?(t('.button_get_started'))).to be(true)
+    expect(page).to have_text(t('home.partials.best_sellers.title_best_sellers'))
+    expect(page).to have_text(t('home.partials.best_sellers.title_best_sellers'))
+    expect(page.has_button?(t('home.partials.carousel.button_buy_now'))).to be(true)
+    expect(page.has_button?(t('home.partials.welcome.button_get_started'))).to be(true)
   end
 end
