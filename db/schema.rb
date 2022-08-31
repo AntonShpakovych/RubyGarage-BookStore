e7cf2b9953d5489cfcd0310f36d7a6d572b8e5f2
AntonShpakovych <shpakovych18@gmail.com>
2022-08-31 16:07:33 +0300

    drop db

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_141940) do
+ActiveRecord::Schema.define(version: 2022_08_23_104253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
-    t.string "city"
-    t.integer "zip"
-    t.string "country"
-    t.string "phone"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "type"
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -80,4 +65,5 @@ ActiveRecord::Schema.define(version: 2022_08_26_141940) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
