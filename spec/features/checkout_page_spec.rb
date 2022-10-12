e92b7f4d93aeca82244ef07fea188139df25287d
AntonShpakovych <shpakovych18@gmail.com>
2022-10-12 15:49:51 +0300

    step 10 done

--- spec/features/checkout_page_spec.rb
+++ spec/features/checkout_page_spec.rb
@@ -294,4 +294,47 @@ RSpec.describe 'Checkout page', type: :feature do
       end
     end
   end
+
+  describe 'Complete' do
+    let(:set_complete) do
+      order.to_delivery!
+      order.to_payment!
+      order.to_confirm!
+      order.to_complete!
+    end
+    let(:delivery) { create(:delivery, orders: [order]) }
+    let(:card) { create(:credit_card, order: order) }
+    let(:billing) { create(:address, :billing_address, user: user) }
+    let(:shipping) { create(:address, :shipping_address, user: user) }
+
+    let(:expected_result_number) { order.number }
+    let(:expected_result_message) { t('checkouts.partials.complete.title') }
+
+    before do
+      delivery
+      card
+      billing
+      shipping
+      set_complete
+      visit checkout_path
+    end
+
+    it 'user can see order number' do
+      expect(result).to have_text(expected_result_number)
+    end
+
+    it 'user can see message' do
+      expect(result).to have_text(expected_result_message)
+    end
+
+    context 'when user click on Go to Store' do
+      before do
+        result.find('a', class: 'btn btn-default mb-20').click
+      end
+
+      it 'redirect_to books_path' do
+        expect(result.current_path).to eq(books_path)
+      end
+    end
+  end
 end
