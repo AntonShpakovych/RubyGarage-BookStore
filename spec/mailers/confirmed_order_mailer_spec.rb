a5dfac83f630f0374567ad9610585de83cac4ac0
AntonShpakovych <shpakovych18@gmail.com>
2022-10-12 15:56:21 +0300

    fixed rspec

--- spec/mailers/confirmed_order_mailer_spec.rb
+++ spec/mailers/confirmed_order_mailer_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ConfirmedOrderMailer, type: :mailer do
   describe '#confirm_order_email' do
     let(:mail) { described_class.confirm_order_email(email) }
     let(:email) { 'test@gmail.com' }
-    let(:expected_result_from) { ENV.fetch('MAIL_HOST') }
+    let(:expected_result_from) { 'fast-temple-25283.herokuapp.com' }
 
     let(:reusult_to) { mail.to }
     let(:expected_result_to) { email }
